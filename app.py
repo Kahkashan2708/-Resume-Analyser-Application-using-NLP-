7dd69977cb4aef079fe42b05e763fe007b3fae25
Kahkashan Manzoor <ma23c014@smail.iitm.ac.in>
2025-07-06 11:07:45 +0530

    added streamlit code

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 
+# loading models
+clf=pickle.load(open('clf.pkl' , "rb"))
+tfidf=pickle.load(open('tfidf.pkl', 'rb'))
+
+# web app
+st.title("Resume Screening App")
